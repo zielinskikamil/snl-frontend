f5c17c387f8737329b600d6d4c7ec1c9cb5d73ff
Leszek Gurniewicz <l.gurniewicz@kainos.com>
2018-05-10 23:13:48 +0200

    bugfix test

--- server.ts
+++ server.ts
@@ -17,9 +17,11 @@ const app = express();
 const PORT = process.env.PORT || 3451;
 const CONFIG = {
   'api_url': process.env['SNL_API_URL'] || 'http://localhost:8090',
-  'dist_folder': process.env['SNL_DIST_FOLDER'] || 'dist'
+  'dist_folder': process.env['SNL_DIST_FOLDER']
 };
 const DIST_FOLDER = join(process.cwd(), CONFIG.dist_folder);
+console.log(DIST_FOLDER);
+console.log('-=-=-=-=-=-==-=-=');
 
 app.use(cors())
 app.options('*', cors()) // include before other routes
